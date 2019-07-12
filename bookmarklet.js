6837ef83c87f789f53bede136647c3134214c4d9
benja <benja@upcu325c3a.local>
2019-07-12 09:54:13 +0200

    Bookmarklet now uses lz-string compression.

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,18 +7,29 @@
 javascript:(
 function() {
     /* URLPages URL */
-    var urlpages_url = 'https://jstrieb.github.io/urlpages/';
-    /* Encode HTML code */
-    var page_data = window.btoa(JSON.stringify({
-        "css" : null,
-        "js" : null,
-        "html" : document.documentElement.outerHTML}));
-    /* If OK was clicked, open Editor with data in new tab and switch to it */
-    if (confirm('Open in Editor?')) {
-        open(`${urlpages_url}editor/#${page_data}`).focus();
-    /* If Cancel was clicked, open URL without Editor */
-    } else {
-        open(`${urlpages_url}#${page_data}`).focus();
-    }
+    var urlpages_url = 'https://benja-johnny.github.io/urlpages/';
+    /* lz-string-1.4.4.js URL */
+    var lz_string_url = urlpages_url + 'lz-string-1.4.4.js';
+    /* Create element for lz-string-1.4.4.js */
+    var lz_script = document.createElement('script');
+    lz_script.setAttribute('type', 'text/javascript');
+    lz_script.setAttribute('charset', 'UTF-8');
+    lz_script.setAttribute('src', lz_string_url);
+    document.documentElement.appendChild(lz_script);
+    /* After loading lz-string-1.4.4.js */
+    lz_script.onload = function() {
+        /* Encode HTML code */
+        var page_data = LZString144.compressToBase64(JSON.stringify({
+            "css" : null,
+            "js" : null,
+            "html" : document.documentElement.outerHTML}));
+        /* If OK was clicked, open Editor with data in new tab and switch to it */
+        if (confirm('Open in Editor?')) {
+            open(`${urlpages_url}editor/#${page_data}`).focus();
+        /* If Cancel was clicked, open URL without Editor */
+        } else {
+            open(`${urlpages_url}#${page_data}`).focus();
+        }
+    };
 }
 )();
